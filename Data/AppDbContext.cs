88b81306946824d4d1933a0e109bb81c541ae62a
Jean Pierre Bouneck <jean-pierre.bouneck@etnic.be>
2024-12-10 15:40:03 +0100

    Adding Context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,6 +5,8 @@ namespace MyFirstWebApi.Data
 {
     public class AppDbContext:DbContext
     {
-        private DbSet<Book> Books { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+        
+        public DbSet<Book> Books { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyFirstWebApi.Data;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Add Dbcontext SQL
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
